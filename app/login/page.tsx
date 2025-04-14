72b568d2481712bf0e06a34f811d214d1d32cb51
Giovanny Bernal <giovas94@gmail.com>
2025-04-14 05:29:31 -0600

    minor changes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,12 +2,15 @@ import Login from "@/app/components/Login";
 import { AuthGetCurrentUserServer } from "../utils/amplify-utils";
 import { redirect } from "next/navigation";
 
-export default async function LoginPage({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | undefined };
+type Params = Promise<{ slug: string }>;
+type SearchParams = Promise<{ [key: string]: string | undefined }>;
+
+export default async function LoginPage(props: {
+  params: Params;
+  searchParams: SearchParams;
 }) {
-  const { next_url } = (await searchParams) || {};
+  const searchParams = await props.searchParams;
+  const next_url = searchParams.next_url;
   const user = await AuthGetCurrentUserServer();
 
   return !user ? (
