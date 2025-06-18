00471a01bbc0c27f7fc51762451af6fa624dd712
Gio Bernav <giobernav@gmail.com>
2025-06-18 14:34:33 +0200

    minor changes

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,7 @@ import { defineBackend } from "@aws-amplify/backend";
 // import { Stack } from "aws-cdk-lib";
 // import { Policy, PolicyStatement, Effect } from "aws-cdk-lib/aws-iam";
 // import { StartingPosition, EventSourceMapping } from "aws-cdk-lib/aws-lambda";
-// import { auth } from "./auth/resource.js";
+import { auth } from "./auth/resource.js";
 // import { data } from "./data/resource.js";
 // import { cognitoGetUserFcn } from "./functions/cognitoGetUser/resource";
 // import { dDBCustomerStreamFcn } from "./functions/dDBCustomerStream/resource";
@@ -12,7 +12,7 @@ import { defineBackend } from "@aws-amplify/backend";
 // import { storage } from "./storage/resource";
 
 const backend = defineBackend({
-  // auth,
+  auth,
   // data,
   // cognitoGetUserFcn,
   // dDBCustomerStreamFcn,
@@ -22,29 +22,29 @@ const backend = defineBackend({
   // storage,
 });
 
-// const { cfnResources } = backend.auth.resources;
-// const { cfnUserPool, cfnUserPoolClient } = cfnResources;
+const { cfnResources } = backend.auth.resources;
+const { cfnUserPool, cfnUserPoolClient } = cfnResources;
 
-// cfnUserPool.addPropertyOverride(
-//   "Policies.SignInPolicy.AllowedFirstAuthFactors",
-//   ["PASSWORD", "WEB_AUTHN", "EMAIL_OTP", "SMS_OTP"]
-// );
+cfnUserPool.addPropertyOverride(
+  "Policies.SignInPolicy.AllowedFirstAuthFactors",
+  ["PASSWORD", "WEB_AUTHN", "EMAIL_OTP", "SMS_OTP"]
+);
 
-// cfnUserPoolClient.explicitAuthFlows = [
-//   "ALLOW_REFRESH_TOKEN_AUTH",
-//   "ALLOW_USER_AUTH",
-// ];
+cfnUserPoolClient.explicitAuthFlows = [
+  "ALLOW_REFRESH_TOKEN_AUTH",
+  "ALLOW_USER_AUTH",
+];
 
-// cfnUserPool.policies = {
-//   passwordPolicy: {
-//     minimumLength: 6,
-//   },
-// };
+cfnUserPool.policies = {
+  passwordPolicy: {
+    minimumLength: 6,
+  },
+};
 
-// cfnUserPool.smsAuthenticationMessage = "Tu codigo de autenticacion es {####}";
-// cfnUserPool.verificationMessageTemplate = {
-//   smsMessage: "Tu codigo de verificacion de tu nueva cuenta es: {####}",
-// };
+cfnUserPool.smsAuthenticationMessage = "Tu codigo de autenticacion es {####}";
+cfnUserPool.verificationMessageTemplate = {
+  smsMessage: "Tu codigo de verificacion de tu nueva cuenta es: {####}",
+};
 
 // const customerTable = backend.data.resources.tables["Customer"];
 // const policy = new Policy(
